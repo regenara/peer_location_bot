9ad2220c4c76eca6a4f059673181be0d6a086796
mstoneho <mstoneho@student.21-school.ru>
2020-12-04 20:03:39 +0300

    dsf

--- intra_auth.py
+++ intra_auth.py
@@ -7,6 +7,11 @@ client_id = data['client_id']
 client_secret = data['client_secret']
 
 
+def requests_get(url, access_token):
+     request = requests.get(url, params={'access_token': access_token}).json()
+     return request
+
+
 def get_token():
     request = requests.post('https://api.intra.42.fr/oauth/token', params={'grant_type': 'client_credentials',
                                                                            'client_id': client_id,
@@ -17,5 +22,28 @@ def get_token():
 
 def get_user(nickname, access_token):
     url = f'https://api.intra.42.fr/v2/users/{nickname}'
-    info = requests.get(url, params={'access_token': access_token}).json()
+    info = requests_get(url, access_token)
     return info
+
+
+def get_user_coalition(user_id, access_token):
+    url = f'https://api.intra.42.fr/v2/users/{user_id}/coalitions_users'
+    coalition_id = requests_get(url, access_token)[0]['coalition_id']
+    url = 'https://api.intra.42.fr/v2/coalitions'
+    get_coalitions = requests_get(url, access_token)
+    coalition = [c['name'] for c in get_coalitions if c['id'] == coalition_id][0]
+    return coalition
+
+
+def get_last_locations(user_id, access_token):
+    url = f'https://api.intra.42.fr/v2/users/{user_id}/location'
+    locations = requests_get(url, access_token)[0]
+    print(locations)
+
+
+t = get_token()
+print(3)
+id_ = get_user('mstoneho', t)['id']
+print(2)
+get_last_locations(id_, t)
+print(1)
