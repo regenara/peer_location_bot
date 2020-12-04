0f09654191bd4b78f18c848a257aecc255c8ef83
mstoneho <mstoneho@student.21-school.ru>
2020-12-04 17:25:22 +0300

    many_user

--- bot.py
+++ bot.py
@@ -10,30 +10,39 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.answer('Привет! Пришли мне ник пользователя и я скажу где он сидит')
+    await message.answer('Привет! Пришли мне ник пользователя и я скажу где он сидит. Можно прислать сразу несколько '
+                         'ников через запятую (не более 5)')
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    nickname = message.text.strip().lower().replace('@', '')
-    token = intra_auth.get_token()
-    get_info = intra_auth.get_user(nickname, token)
-    text = 'Пользователь не найден! Проверь правильность введенных данных'
-    if get_info:
-        displayname = get_info['displayname']
-        login = get_info['login']
-        cursus_users = get_info['cursus_users']
-        cursus_info = '\n'.join([f'<b>{c["cursus"]["name"]}:</b> {round(c["level"], 2)}' for c in cursus_users])
-        campus = get_info['campus'][0]['name']
-        image_url = get_info['image_url']
-        location = get_info['location']
-        if location is None:
-            location = 'Не в кампусе'
-        if get_info['staff?'] and location is None:
-            location = 'Спроси в АДМ'
-        text = f'<b>{displayname}</b> aka {login}\n{cursus_info}\n<b>Кампус:</b> {campus}\n<b>Место:</b> {location}' \
-               f'<a href="{image_url}">​</a>'
-    await message.answer(text)
+    nicknames = message.text.strip().lower().replace('@', '').replace(' ', '').split(',')
+    texts = []
+    alone = True
+    if len(nicknames) > 1:
+        alone = False
+    for nickname in nicknames[:5]:
+        nickname = nickname.strip()
+        token = intra_auth.get_token()
+        get_info = intra_auth.get_user(nickname, token)
+        text = f'Пользователь <b>{nickname}</b> не найден! Проверь правильность введенных данных'
+        if get_info:
+            displayname = get_info['displayname']
+            login = get_info['login']
+            cursus_users = get_info['cursus_users']
+            cursus_info = '\n'.join([f'<b>{c["cursus"]["name"]}:</b> {round(c["level"], 2)}' for c in cursus_users])
+            campus = get_info['campus'][0]['name']
+            image_url = get_info['image_url']
+            location = get_info['location']
+            if location is None:
+                location = 'Не в кампусе'
+            if get_info['staff?'] and location is None:
+                location = 'Спроси в АДМ'
+            text = f'<b>{displayname}</b> aka {login}\n{cursus_info}\n<b>Кампус:</b> {campus}\n<b>Место:</b> {location}'
+            if alone:
+                text += f'<a href="{image_url}">​</a>'
+        texts.append(text)
+    await message.answer(f'\n➖➖➖➖➖➖➖➖➖➖\n'.join(texts))
 
 
 if __name__ == '__main__':
